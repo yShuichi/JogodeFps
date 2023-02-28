8e588d877bf665db5a61b2077d01ba3aaf2249f4
Samuel Roberto <samuel.rcc467@gmail.com>
2023-02-28 18:32:18 -0300

    Jogo3D
    dwd

---
+++ Projeto3D/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform cameraPos;
+
+    void Update()
+    {
+        
+        transform.position =  cameraPos.position;
+
+    }
+}
